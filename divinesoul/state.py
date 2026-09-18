c0ec0d73c5665005e17d79114ab284a40a13974b
soldiv86-rgb <soldiv86@gmail.com>
2026-09-19 06:30:09 +0800

    Delete divinesoul directory

--- divinesoul/state.py
+++
@@ -1,6 +0,0 @@
-"""Shared runtime state (accounts + theme + UI settings dicts)."""
-from .config import DEFAULT_THEME
-
-accounts = {}  # key -> account report dict
-theme = dict(DEFAULT_THEME)
-ui_settings = {}  # cached UI settings; filled by persistence.init_ui_settings()
